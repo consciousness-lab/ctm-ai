af83e33ebf49af9ca223b4c225b37805fc938074
Haofei Yu <1125027232@qq.com>
2024-06-24 23:40:25 -0400

    support wolfram alpha and search engine (#31)
    * support wolfram alpha and search engine
    
    * delete deprecated processors

--- ctm/processors/__init__.py
+++ ctm/processors/__init__.py
@@ -1,9 +1,13 @@
 from .processor_base import BaseProcessor
 from .processor_gpt4 import GPT4Processor
 from .processor_gpt4v import GPT4VProcessor
+from .processor_search_engine import SearchEngineProcessor
+from .processor_wolfram_alpha import WolframAlphaProcessor
 
 __all__ = [
     "BaseProcessor",
     "GPT4VProcessor",
     "GPT4Processor",
+    "SearchEngineProcessor",
+    "WolframAlphaProcessor",
 ]
---
+++ ctm/processors/processor_search_engine.py
@@ -0,0 +1,29 @@
+from typing import Any, Optional
+
+from ..executor.executor_base import BaseExecutor
+from ..messengers.messenger_base import BaseMessenger
+from ..scorers.scorer_base import BaseScorer
+from .processor_base import BaseProcessor
+
+
+@BaseProcessor.register_processor("search_engine_processor")
+class SearchEngineProcessor(BaseProcessor):
+    def __init__(
+        self,
+        name: str,
+        group_name: Optional[str] = None,
+        *args: Any,
+        **kwargs: Any
+    ) -> None:
+        super().__init__(name, group_name, *args, **kwargs)
+
+    def init_messenger(self) -> None:
+        self.messenger = BaseMessenger(
+            messenger_name="search_engine_messenger"
+        )
+
+    def init_executor(self) -> None:
+        self.executor = BaseExecutor(executor_name="search_engine_executor")
+
+    def init_scorer(self) -> None:
+        self.scorer = BaseScorer(scorer_name="gpt4_scorer")
