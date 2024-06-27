ff9f2cb774817d311f0e033632c666eb3d0c7ff4
Haofei Yu <1125027232@qq.com>
2024-06-27 14:55:04 -0400

    run ruff

--- examples/sarcasm_detection.py
+++ examples/sarcasm_detection.py
@@ -1,8 +1,9 @@
 import sys
 
-sys.path.append('..')
 from ctm_ai.ctms.ctm_base import BaseConsciousnessTuringMachine
 from ctm_ai.utils import load_image
+
+sys.path.append('..')
 
 if __name__ == '__main__':
     ctm = BaseConsciousnessTuringMachine('sarcasm_ctm')
