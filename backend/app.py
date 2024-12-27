ac249dfab7f917a33242850751774652899db54b
Haofei Yu <1125027232@qq.com>
2024-12-28 01:04:05 +0800

    feat(backend, frontend): support init complete frontend and backend (#125)

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, make_response, request
+
+app = Flask(__name__)
+
+@app.route('/api/nodes/<node_id>')
+def get_node_details(node_id):
+    # Build your response here...
+    response_data = {"details": f"This is node {node_id}"}
+    response = make_response(jsonify(response_data))
+    
+    # Add CORS headers:
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
+    response.headers.add("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+
+    return response
+
+if __name__ == '__main__':
+    app.run(port=5000)
