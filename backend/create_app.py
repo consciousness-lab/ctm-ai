43314333ca5efdfa9a3b5ef09a9e472e8be280ce
Yining Zhao <zhaoyining5445@gmail.com>
2025-01-17 04:53:13 -0600

    fix mypy error

--- backend/create_app.py
+++ backend/create_app.py
@@ -1,7 +1,9 @@
+from flask import Flask
+
 from app_wrapper import FlaskAppWrapper
 
 
-def create_app():
+def create_app() -> Flask:
     """Factory function to create and configure the Flask application."""
     app_wrapper = FlaskAppWrapper()
     return app_wrapper.app
