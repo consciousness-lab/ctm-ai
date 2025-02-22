dc4e62a016357a0ea6afe093fe8fd9f9b5ed4c9e
Haofei Yu <1125027232@qq.com>
2025-02-22 17:34:06 -0600

    fix(backend): not callable error fix (#215)
    Co-authored-by: lwaekfjlk <ubuntu@ip-172-31-13-120.us-east-2.compute.internal>

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,15 @@
 from app_wrapper import FlaskAppWrapper
 from flask_cors import CORS
 
-app = FlaskAppWrapper()
-CORS(app.app, origins=['http://localhost:3000', 'http://18.224.61.142'])
+# Create an instance of your FlaskAppWrapper
+flask_wrapper = FlaskAppWrapper()
+
+# Enable CORS on the underlying Flask app
+CORS(flask_wrapper.app, origins=['http://localhost:3000', 'http://18.224.61.142'])
+
+# Export the underlying Flask app as the global WSGI callable.
+app = flask_wrapper.app
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
