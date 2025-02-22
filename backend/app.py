4127c7ba37c883a9d0182b5c1757c886ba5e1d47
Ubuntu <ubuntu@ip-172-31-13-120.us-east-2.compute.internal>
2025-02-22 18:28:40 +0000

    update

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from app_wrapper import FlaskAppWrapper
 
 app = FlaskAppWrapper()
-CORS(app)
+CORS(app.app)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
