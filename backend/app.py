a7eaeab2b8ac33eb6bacbeeb2acde5f042ad2d96
Haofei Yu <1125027232@qq.com>
2025-02-22 12:21:12 -0600

    update

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,9 @@
+from flask import Flask
+from flask_cors import CORS
 from app_wrapper import FlaskAppWrapper
 
+app = FlaskAppWrapper()
+CORS(app)
+
 if __name__ == '__main__':
-    app = FlaskAppWrapper()
     app.run(port=5000, debug=True)
