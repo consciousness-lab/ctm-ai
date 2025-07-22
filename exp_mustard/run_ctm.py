bc54d8c4cf54e5ef8f3195e43a4c5ea87ebe0184
Yining Zhao <zhaoyining5445@gmail.com>
2025-07-23 02:08:04 +0800

    exp(prompt): add prompt

--- exp_mustard/run_ctm.py
+++ exp_mustard/run_ctm.py
@@ -4,36 +4,40 @@ import sys
 
 from ctm_ai.ctms.ctm import ConsciousnessTuringMachine
 
-sys.path.append('..')
+sys.path.append("..")
 
 SYS_PROMPT = (
-    'Please analyze the inputs provided to determine the punchline provided sarcasm or not.'
-    "Your answer should start with 'Yes' or 'No'."
-    "If you think these inputs includes exaggerated description or its real meaning is not aligned with the original one, please answer 'Yes'."
-    "If you think these inputs is neutral or its true meaning is not different from its original one, please answer 'No'."
-    'You should also provide your reason for your answer.'
+    "Please analyze the inputs provided to determine the person is sarcasm or not."
+    "You should also provide your reason for your answer."
+)
+LANGUAGE_DESCRIPTION = (
+    "Additional instructions to judge the text is sarcasm or not:"
+    "If you think the text includes exaggerated description or includes strong emotion or its real meaning is not aligned with the original one, it usually means sarcasm."
+    "If you think the text is neutral or its true meaning is not different from its original one, it usually means not sarcasm."
+    "Please make sure that your answer is based on the text itself, not on the context or your personal knowledge."
+    "You should only make sarcasm judgement when you are very sure that the text is sarcastic."
 )
 
 
 def load_data(file_path):
-    with open(file_path, 'r', encoding='utf-8') as json_file:
+    with open(file_path, "r", encoding="utf-8") as json_file:
         data = json.load(json_file)
     return data
 
 
-def run_instance(test_file, output_file='ctm.jsonl'):
-    dataset = load_data('mustard_dataset/mustard_dataset_test.json')
-    ctm = ConsciousnessTuringMachine('sarcasm_ctm')
-    target_sentence = dataset[test_file]['utterance']
-    query = f"{SYS_PROMPT}\n\n punchline:'{target_sentence}' "
-    text_list = dataset[test_file]['context']
+def run_instance(test_file, output_file="ctm.jsonl"):
+    dataset = load_data("mustard_dataset/mustard_dataset_test.json")
+    ctm = ConsciousnessTuringMachine("sarcasm_ctm")
+    target_sentence = dataset[test_file]["utterance"]
+    query = SYS_PROMPT
+    text_list = dataset[test_file]["context"]
     text_list.append(target_sentence)
-    fullContext = ''
+    fullContext = f"{LANGUAGE_DESCRIPTION}\n\n The full context is: \n"
     for i in range(len(text_list)):
-        currentUtterance = f'{text_list[i]} \n'
+        currentUtterance = f"{text_list[i]} \n"
         fullContext += currentUtterance
-    audio_path = f'mustard_audios/{test_file}_audio.mp4'
-    video_frames_path = f'mustard_frames/{test_file}_frames'
+    audio_path = f"mustard_audios/{test_file}_audio.mp4"
+    video_frames_path = f"mustard_frames/{test_file}_frames"
     file_paths = [
         os.path.join(video_frames_path, file_name)
         for file_name in os.listdir(video_frames_path)
@@ -42,32 +46,38 @@ def run_instance(test_file, output_file='ctm.jsonl'):
     answer = ctm(
         query=query,
         text=fullContext,
-        video_frames_path=file_paths,
-        audio_path=audio_path,
+        # video_frames_path=file_paths,
+        # audio_path=audio_path,
     )
 
-    print('------------------------------------------')
+    print("------------------------------------------")
     print(answer)
-    print('------------------------------------------')
+    print("------------------------------------------")
 
     result = {
         test_file: {
-            'answer': [answer],
-            'label': dataset[test_file]['sarcasm'],
+            "answer": [answer],
+            "label": dataset[test_file]["sarcasm"],
         }
     }
 
-    with open(output_file, 'a', encoding='utf-8') as f:
-        f.write(json.dumps(result, ensure_ascii=False) + '\n')
+    with open(output_file, "a", encoding="utf-8") as f:
+        f.write(json.dumps(result, ensure_ascii=False) + "\n")
 
 
-if __name__ == '__main__':
-    dataset_path = 'mustard_dataset/mustard_dataset_test.json'
+if __name__ == "__main__":
+    dataset_path = "mustard_dataset/mustard_dataset_test.json"
     dataset = load_data(dataset_path)
 
     test_list = list(dataset.keys())
-    print(f'Total Test Cases: {len(test_list)}')
-    test_list = test_list[:1]
+    print(f"Total Test Cases: {len(test_list)}")
+    test_list = test_list[:100]
+    false_list = []
 
     for test_file in test_list:
+        if not dataset[test_file]["sarcasm"]:
+            false_list.append(test_file)
+
+    print(f"Total False Cases: {len(false_list)}")
+    for test_file in false_list:
         run_instance(test_file)
