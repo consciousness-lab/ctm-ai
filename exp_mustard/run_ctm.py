cbaa34f31b7544b0d7ed9f0e8ed2873acd2425d8
Yining Zhao <zhaoyining5445@gmail.com>
2025-07-23 03:33:05 +0800

    feat(config): support more system prompt for different modalities

--- exp_mustard/run_ctm.py
+++ exp_mustard/run_ctm.py
@@ -4,24 +4,24 @@ import sys
 
 from ctm_ai.ctms.ctm import ConsciousTuringMachine
 
-sys.path.append('..')
+sys.path.append("..")
 
 
 def load_data(file_path):
-    with open(file_path, 'r', encoding='utf-8') as json_file:
+    with open(file_path, "r", encoding="utf-8") as json_file:
         data = json.load(json_file)
     return data
 
 
-def run_instance(test_file, output_file='ctm.jsonl'):
-    dataset = load_data('mustard_dataset/mustard_dataset_test.json')
-    ctm = ConsciousTuringMachine('sarcasm_ctm')
-    target_sentence = dataset[test_file]['utterance']
-    query = 'Is the person saying sarcasm or not?'
-    text_list = dataset[test_file]['context']
+def run_instance(test_file, output_file="ctm.jsonl"):
+    dataset = load_data("mustard_dataset/mustard_dataset_test.json")
+    ctm = ConsciousTuringMachine("sarcasm_ctm")
+    target_sentence = dataset[test_file]["utterance"]
+    query = "Is the person saying sarcasm or not?"
+    text_list = dataset[test_file]["context"]
     text_list.append(target_sentence)
-    audio_path = f'mustard_audios/{test_file}_audio.mp4'
-    video_frames_path = f'mustard_frames/{test_file}_frames'
+    audio_path = f"mustard_audios/{test_file}_audio.mp4"
+    video_frames_path = f"mustard_frames/{test_file}_frames"
     file_paths = [
         os.path.join(video_frames_path, file_name)
         for file_name in os.listdir(video_frames_path)
@@ -34,27 +34,27 @@ def run_instance(test_file, output_file='ctm.jsonl'):
         audio_path=audio_path,
     )
 
-    print('------------------------------------------')
+    print("------------------------------------------")
     print(answer)
-    print('------------------------------------------')
+    print("------------------------------------------")
 
     result = {
         test_file: {
-            'answer': [answer],
-            'label': dataset[test_file]['sarcasm'],
+            "answer": [answer],
+            "label": dataset[test_file]["sarcasm"],
         }
     }
 
-    with open(output_file, 'a', encoding='utf-8') as f:
-        f.write(json.dumps(result, ensure_ascii=False) + '\n')
+    with open(output_file, "a", encoding="utf-8") as f:
+        f.write(json.dumps(result, ensure_ascii=False) + "\n")
 
 
-if __name__ == '__main__':
-    dataset_path = 'mustard_dataset/mustard_dataset_test.json'
+if __name__ == "__main__":
+    dataset_path = "mustard_dataset/mustard_dataset_test.json"
     dataset = load_data(dataset_path)
 
     test_list = list(dataset.keys())
-    print(f'Total Test Cases: {len(test_list)}')
+    print(f"Total Test Cases: {len(test_list)}")
     test_list = test_list[:10]
 
     for test_file in test_list:
